188cb8c6e7ec12f24eed760d0c9afa1cf3aa7525
nixzus <61649947+yannnaingoo@users.noreply.github.com>
2022-12-30 00:44:15 +0630

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,7 +1,7 @@
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import axios from "axios";
+// import Head from "next/head";
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
+// import axios from "axios";
 
 // export default function Post({ post }) {
 //   const router = useRouter();
